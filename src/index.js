f5ff1803ef44b2b444953e96ca6f37c583600487
David Mendoza <d.mendoza99@unitec.edu>
2021-02-09 20:06:59 +0000

    TDD pair session finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-const sum = (a, b) => {
-  return a + b;
+class Calculator {
+    Sum(numbers) {
+        if (numbers.length === 0) {
+            return 0;
+        }
+
+        let delimiter = undefined;
+        if (numbers.includes("//")) {
+            numbers = numbers.replace("//", "");
+            delimiter = numbers[0];
+        }
+
+        const listOfNumbers = numbers.split("").map(char =>[delimiter, ",", "\n"].includes(char) ? "," : char).join("").split(",").map(n => Number(n));
+
+        const negatives = listOfNumbers.filter((number) => number < 0);
+
+        if (negatives.length > 0) {
+            throw new Error(`negatives not allowed: ${negatives.join(", ")}`)
+        }
+
+        const sumOfAll = listOfNumbers.reduce((pv, cv) => pv + cv, 0);
+        return sumOfAll;
+    }
 }
 
-module.exports = sum;
+module.exports = Calculator;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,51 @@
 /* eslint-disable no-undef */
-const sum = require('../src/index.js');
+const calc = require("../src/index");
 
 describe('it should add 1 + 2', () => {
-    it("should add to 3", () => {
-        expect(sum(1, 2)).toBe(3)
+    const calculator = new calc();
+    it("Empty string should return 0", () => {
+        expect(calculator.Sum("")).toBe(0);
     });
-    it("should add to 4", () => {
-        expect(sum(1, -3)).toBe(4)
+
+    it("When input is one number should return the same number", () => {
+        expect(calculator.Sum("3")).toBe(3);
+    })
+
+    it("When input is two numbers should return the sum of the numbers", () => {
+        expect(calculator.Sum("1,3")).toBe(4);
+    })
+
+    it("When input is any amount of numbers should return the sum of all those numbers", () => {
+        expect(calculator.Sum("1,2,3,4")).toBe(10);
+    })
+
+    it("When input has diferent delimeters for the numbers it should return the sum of all those numbers", () => {
+        expect(calculator.Sum("1,2\n3,4")).toBe(10);
+    })
+
+    it("When using a custom character as delimiter it should return the sum of all numbers", () => {
+        expect(calculator.Sum("//.\n1.2.3")).toBe(6);
+    })
+
+    it("When input includes a negative number it should return a exception showing the negative number", () => {
+        expect(() => { calculator.Sum("//.\n1.-2.3") }).toThrow("negatives not allowed: -2");
+    })
+
+    it("When input includes several negative numbers it should return a exception showing all negatives", () => {
+        expect(() => { calculator.Sum("//.\n1.-2.-3") }).toThrow("negatives not allowed: -2, -3");
     })
 });
+
+/*
+1. Given the user input is empty when calculating the sum then it should return zero.
+2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
+3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
+6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
+7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
+8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
+9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
+10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
+11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6)
+*/
